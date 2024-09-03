26b41472c895956954158ee2ac86d1684f715a9e
Wilhitesa <Wilhitesa@gmail.com>
2024-09-02 18:56:28 -0700

    Primary functionality completed, need to implement file writing.

--- main.py
+++ main.py
@@ -1,16 +1,40 @@
-# This is a sample Python script.
+# Press ⌃R to execute it.
+import csv
 
-# Press ⌃R to execute it or replace it with your code.
-# Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
+# Important initializations. used for filenames.
+fulldata_filename = 'FullNormalData.csv'
+filter_filename = 'DataFilter.csv'
+final_filename = 'RemainderRows.csv'
 
+#fields = []
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press ⌘F8 to toggle the breakpoint.
+with open(fulldata_filename, 'r') as fulldata_file:
+    # Create CSV file. Find the index of the ID field for future use.
+    fulldata_csv = csv.reader(fulldata_file)
+    fields = next(fulldata_csv)
+    id_index_full = fields.index('ASU ID')
 
+    # Iterate through each row in the full data
+    for row in fulldata_csv:
+        # Retrieve the ID in the row.
+        #fulldata_row = next(fulldata_csv)
+        fulldata_id = row[id_index_full]
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
+        # Open the filter file. Find the index of the ID field as well.
+        with open(filter_filename, 'r') as filter_file:
+            filterdata_csv = csv.reader(filter_file)
+            filter_fields = next(filterdata_csv)
+            id_index_filtered = filter_fields.index('ASU ID')
+            in_filter = False
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+            # Run through the entire file. If the IDS match, move to the next name.
+            # In other words, name already exists in the filter file.
+            for filter_row in filterdata_csv:
+                #filterdata_row = next(filterdata_csv)
+                filterdata_id = filter_row[id_index_filtered]
+                if(fulldata_id == filterdata_id):
+                    in_filter = True
+                    break
+
+            if not in_filter:
+                print(row)
